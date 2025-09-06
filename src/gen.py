c08b963314025a4bf919049d7d7fc4037ab427df
James Chang <jc@quantrend.ai>
2025-09-06 23:06:36 +0800

    fix: include -> input

--- src/gen.py
+++ src/gen.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import json
 
 def __include(fnames = []):
-	return ['\\include{' + fname + "}" for fname in fnames]
+	return ['\\input{' + fname + "}" for fname in fnames]
 
 def __chapter(chapter):
 	return ['\\chapter{' + chapter + '}']
@@ -26,7 +26,7 @@ def _gen(fnames = [], chapter = "tmp"):
 	cname = chapter + ".tex"
 	with open("../tex/template.tex", "r") as fd:
 		template = [l.strip() for l in fd.readlines()]
-	
+
 	output = []
 	for t in template:
 		if t == "%%% include":
@@ -42,7 +42,7 @@ def _gen_main(fnames = {}, cname = "main"):
 	cname = cname + ".tex"
 	with open("../tex/template.tex", "r") as fd:
 		template = [l.strip() for l in fd.readlines()]
-	
+
 	fnames = fnames.copy()
 	output = []
 	for t in template:
@@ -62,12 +62,12 @@ def _gen_main(fnames = {}, cname = "main"):
 
 	with open(join(OUTPUT_DIR, cname), "w") as fd:
 		fd.writelines(l + "\n" for l in output)
-	
+
 
 def gen(dir = "../tex"):
 	chapters = listdir(dir)
 	allFnames = {}
-	
+
 	for chapter in chapters:
 		fnames = []
 		if (chapter in IGNORE_DIR) or Path(join(dir, chapter)).is_file():
