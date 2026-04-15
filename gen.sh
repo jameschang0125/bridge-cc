85271d4d51fadc4f1a756ead4e1105e78e5065b4
capoouo <capoouo@capoouos-MacBook-Pro.local>
2026-04-15 11:10:46 +0800

    refactor opening

--- gen.sh
+++ gen.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
-rm -r tex-tmp
-rm tex-book/*
-if test -d tex-tmp; then
-	echo "exited since tex-tmp exists"
-	exit
+set -u
+
+target="${1:-all}"
+
+if [[ "$target" == "all" ]]; then
+	targets=(main.tex opening.tex conventions.tex principles.tex defensive.tex summary.tex)
+else
+	target_file="${target%.tex}.tex"
+	if [[ ! -f "tex-book/$target_file" ]]; then
+		echo "unknown target: $target"
+		echo "available targets: all, main, opening, conventions, principles, defensive, summary"
+		exit 1
+	fi
+	targets=("$target_file")
 fi
+
+rm -rf tex-tmp
 mkdir tex-tmp
+
 cd src
 python gen.py
 cd ..
+
 cp -r tex tex-tmp
 cp -r tex-book tex-tmp
+
 cd tex-tmp
 mkdir log
 cd tex-book
-for f in *.tex; do
+
+for f in "${targets[@]}"; do
 	echo
 	echo ===== compiling $f =====
-	echo X | time xelatex --halt-on-error $f > ../log/$f.log1
+	echo X | time xelatex --halt-on-error "$f" > "../log/$f.log1"
 	if (( $? )); then
-		echo error when compiling $f, exiting...
-		cat ../log/$f.log1
-		exit
+		echo "error when compiling $f, exiting..."
+		cat "../log/$f.log1"
+		exit 1
 	fi
-	echo X | time xelatex --halt-on-error $f > ../log/$f.log2
+	echo X | time xelatex --halt-on-error "$f" > "../log/$f.log2"
 	if (( $? )); then
-		echo error when compiling $f, exiting...
-		cat ../log/$f.log2
-		exit
+		echo "error when compiling $f, exiting..."
+		cat "../log/$f.log2"
+		exit 1
 	fi
-	echo X | time xelatex --halt-on-error $f > ../log/$f.log3
+	echo X | time xelatex --halt-on-error "$f" > "../log/$f.log3"
 	if (( $? )); then
-		echo error when compiling $f, exiting...
-		cat ../log/$f.log3
-		exit
+		echo "error when compiling $f, exiting..."
+		cat "../log/$f.log3"
+		exit 1
 	fi
 done
+
 cd ../..
 mkdir -p tex-result
 cp tex-tmp/tex-book/*.pdf tex-result
